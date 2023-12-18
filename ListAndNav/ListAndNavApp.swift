3c7f9f70e997c3c24f12dae00fb4a93c7ef3827b
OlegPlugaru <olegplugaru.md@gmail.com>
2023-12-18 20:00:20 +0200

    Initial Commit

---
+++ ListAndNav/ListAndNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAndNavApp.swift
+//  ListAndNav
+//
+//  Created by Oleg Plugaru on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ListAndNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
